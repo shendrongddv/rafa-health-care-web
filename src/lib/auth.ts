fcb0aa916efb35cf2163c4643ff9900fc44662b7
Kang Shend Rong <shendrongddv@gmail.com>
2025-01-24 16:38:32 +0700

    setup auth system

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,8 +1,10 @@
-import NextAuth from "next-auth"
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import { prisma } from "@/lib/prisma"
- 
-export const { handlers, auth, signIn, signOut } = NextAuth({
+import NextAuth from "next-auth";
+import Github from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "./prisma";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [],
-})
+  providers: [Github, Google],
+});
